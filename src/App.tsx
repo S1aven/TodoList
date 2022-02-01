fb39556e361429e86ed169adc32655a6a50ba1a1
Sergei <rudnev09@mail.ru>
2022-02-01 20:03:55 +0300

    'DotoList'

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './App.css';
+import TodoList, {TaskType} from "./Components/TodoList";
+
+function App() {
+
+  const tasks_1: Array<TaskType> = [
+    {id: 1, title: 'HTML', isDone: true},
+    {id: 2, title: 'CSS', isDone: true},
+    {id: 3, title: 'JS/TS', isDone: false}
+  ]
+
+  const tasks_2: Array<TaskType> = [
+    {id: 1, title: 'HTML', isDone: true},
+    {id: 2, title: 'CSS', isDone: true},
+    {id: 3, title: 'JS/TS', isDone: true}
+  ]
+
+  const tasks_3: Array<TaskType> = [
+    {id: 1, title: 'HTML', isDone: true},
+    {id: 2, title: 'CSS', isDone: true},
+    {id: 3, title: 'JS/TS', isDone: true}
+  ]
+
+  return (
+    <div className="App">
+      <TodoList title={'What to learn'} tasks={tasks_1}/>
+      <TodoList title={'What to read'} tasks={tasks_2}/>
+      <TodoList title={'what to duy'} tasks={tasks_3}/>
+    </div>
+  );
+}
+
+export default App;
